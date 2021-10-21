45e52d959dcf3a824dfc438fabd5260d5bf1e566
Gonzalo Rubio <gonzrubio95@gmail.com>
2021-10-21 10:32:33 -0700

    Load vgg 19.

--- Neural_Style_Transfer_Gatys_et_al_2015/driver.py
+++ Neural_Style_Transfer_Gatys_et_al_2015/driver.py
@@ -9,12 +9,18 @@ Created on Wed Oct 20 13:38:44 2021
 
 import torch
 import torch.nn as nn
+import torchvision
 
 
 # initialize pretrained vgg19 or larger
+# vgg = models.vgg19(pretrained=True)
+vgg = torchvision.models.vgg19(pretrained=True)
+
 # remove fc layers and freeze conv layers
 # data: 3 images (random for now), image sc them
 # Get output from all conv_x-1 layers for all three inputs
 # put all 3xnumb_conv_x-1 layers outputs in the cost function
 # define loss function and update pixel values?
+
+
 
