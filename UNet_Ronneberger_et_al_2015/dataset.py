0caac79e71e6d5e4fd522f01c95176266a4aed46
Gonzalo Rubio <gonzrubio95@gmail.com>
2021-11-05 15:32:48 -0700

    Link to dataset.

---
+++ UNet_Ronneberger_et_al_2015/dataset.py
@@ -0,0 +1,8 @@
+"""U-Net: Convolutional Networks for Biomedical Image Segmentation
+
+dataset: https://www.ebi.ac.uk/biostudies/studies/S-BSST265
+
+Created on Tue Nov  2 18:27:52 2021
+
+@author: gonzr
+"""
