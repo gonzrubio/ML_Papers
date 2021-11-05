f78a8ebefbc5ad090c647bfeb7da1b6bfc70bc90
Gonzalo Rubio <gonzrubio95@gmail.com>
2021-11-04 20:39:01 -0700

    pool in forward of ConvBlock.

--- UNet_Ronneberger_et_al_2015/model.py
+++ UNet_Ronneberger_et_al_2015/model.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 
 class ConvBlock(nn.Module):
     def __init__(self, in_channels=1, hidden_channels=64, out_channels=64):
-        super( ConvBlock, self).__init__()
+        super(ConvBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels,
                                out_channels=hidden_channels,
                                kernel_size=3, stride=1, padding=0, bias=False)
@@ -35,22 +35,35 @@ class ConvBlock(nn.Module):
         x = self.conv2(x)
         x = self.batch_norm2(x)
         x = self.relu(x)
+        x = self.pool(x)
 
         return x
 
 
 class UNet(nn.Module):
-    def __init__(self):
-        super(self, UNet).__init__()
+    def __init__(self, in_channels=1, out_channels=1):
+        super(UNet, self).__init__()
         # Downsample conv blocks 5 total, from top to bottom
-        # Copy crop for skip connections:
+        self.features = 64
+        self.contract = nn.ModuleList()
+        self.contract.append(ConvBlock(in_channels=in_channels,
+                                       hidden_channels=self.features,
+                                       out_channels=self.features))
+        # self.expand = nn.ModuleList()
+
         # one idea would be to upsample so it matches the shape of the other
         # side of the U, no pixels are dropped that way
+
         # Upsample conv blocks 4 total, from bottom to top)
         # read about trsnpose conv
         # concat skip connections along channels dimension
         # output segmentation map
 
+    def forward(self, x):
+
+        x = self.contract[0](x)
+        # Copy crop for skip connections:
+        return x
 
 if __name__ == '__main__':
     # downsample conv
@@ -66,8 +79,42 @@ if __name__ == '__main__':
     # print(x.shape)
     
     # output conv
-    x = torch.randn((1, 128, 392, 392))
-    conv_block = ConvBlock(in_channels=128, hidden_channels=64, out_channels=64)
-    x = conv_block(x)
-    print(x.shape)    
-    
+    # x = torch.randn((1, 128, 392, 392))
+    # conv_block = ConvBlock(in_channels=128, hidden_channels=64, out_channels=64)
+    # x = conv_block(x)
+    # print(x.shape)
+
+    # contracting path
+    x = torch.randn((1, 1, 572, 572)) 
+    model = UNet()
+    x_out = model(x)
+    print(x_out.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
