f73c166493565c4b91ad1a15a91258522036eb80
Gonzalo Rubio <gonzrubio95@gmail.com>
2021-11-22 22:46:39 -0800

    Apply transforms to train and val data.

--- Pix2Pix_Isola_et_al_2017/dataset.py
+++ Pix2Pix_Isola_et_al_2017/dataset.py
@@ -8,7 +8,6 @@ Created on Mon Nov 22 17:15:42 2021
 """
 
 
-import numpy as np
 import os
 import torch
 
@@ -24,7 +23,7 @@ torch.cuda.empty_cache()
 
 def mean_std(dataset):
     """Return the mean and std of the dataset."""
-    loader = DataLoader(dataset, batch_size=32, num_workers=0, shuffle=False)
+    loader = DataLoader(dataset, batch_size=128, num_workers=0, shuffle=False)
 
     mean_inputs = 0.
     std_inputs = 0.
@@ -49,64 +48,67 @@ def mean_std(dataset):
     return (mean_inputs, std_inputs), (mean_targets, std_targets)
 
 
-class ComicFaces(Dataset):
-    """A paired face to comics dataset."""
+class Face2Comic(Dataset):
+    """A paired face-to-comics dataset."""
 
-    def __init__(self, data_dir, transform_faces=None, transform_comics=None):
-        super(ComicFaces, self).__init__()
+    def __init__(self, data_dir, train=True):
+        super(Face2Comic, self).__init__()
         self.data_dir = data_dir
         self.faces_dir = os.path.join(data_dir, "faces")
         self.faces = os.listdir(self.faces_dir)
         self.comics_dir = os.path.join(data_dir, "comics")
         self.comics = os.listdir(self.comics_dir)
-        self.transform_faces = transform_faces
-        self.transform_comics = transform_comics
         self.len = len(self.faces)
+        self.train = train
+
+    def apply_transforms(self, face, comic):
+        """Apply the same transforms to the input and the target."""
+        common_transform = transforms.Compose([transforms.Resize((256, 256)),
+                                               transforms.ToTensor()])
+
+        normalize_face = transforms.Normalize(mean=[0.5129, 0.4136, 0.3671],
+                                              std=[0.2372, 0.1972, 0.1883])
+
+        normalize_comic = transforms.Normalize(mean=[0.4445, 0.3650, 0.3226],
+                                               std=[0.2594, 0.2051, 0.1840])
+
+        face = normalize_face(common_transform(face))
+        comic = normalize_comic(common_transform(comic))
+
+        if self.train:
+            train_transform = transforms.Compose([
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ColorJitter()])
+            face = train_transform(face)
+            comic = train_transform(comic)
+
+        return face, comic
 
     def __len__(self):
         """Get the number of samples in the dataset."""
         return self.len
 
     def __getitem__(self, index):
-        """Return the input (face) and the target (comic)."""
+        """Return the transformed input (face) and target (comic)."""
         face = Image.open(os.path.join(self.faces_dir, self.faces[index]))
-        if self.transform_faces:
-            face = self.transform_faces(face)
-
         comic = Image.open(os.path.join(self.comics_dir, self.comics[index]))
-        if self.transform_comics:
-            comic = self.transform_comics(comic)
-
-        return face, comic
+        return self.apply_transforms(face, comic)
 
 
 if __name__ == '__main__':
 
     data_dir_train = os.getcwd() + '\\data\\train\\'
-
-    # TO DO: Look at albumentations docs and Aladdin and the transforms done
-    # in the paper.
-    transform_faces = transforms.Compose(
-        [transforms.Resize((256, 256)),
-         transforms.ToTensor(),
-         transforms.Normalize(mean=[0.5129, 0.4136, 0.3671],
-                              std=[0.2372, 0.1972, 0.1883])
-         ])
-
-    transform_comics = transforms.Compose(
-        [transforms.Resize((256, 256)),
-         transforms.ToTensor(),
-         transforms.Normalize(mean=[0.4445, 0.3650, 0.3226],
-                              std=[0.2594, 0.2051, 0.1840])
-         ])
-
-    dataset_train = ComicFaces(
-        data_dir=data_dir_train,
-        transform_faces=transform_faces,
-        transform_comics=transform_comics
-        )
+    dataset_train = Face2Comic(data_dir=data_dir_train, train=True)
 
     stats_faces, stats_comics = mean_std(dataset_train)
+
+    print(f"Faces: mean = {stats_faces[0]}, std = {stats_faces[1]}")
+    print(f"Comics: mean = {stats_comics[0]}, std = {stats_comics[1]}")
+
+    data_dir_val = os.getcwd() + '\\data\\val\\'
+    dataset_val = Face2Comic(data_dir=data_dir_val, train=False)
+
+    stats_faces, stats_comics = mean_std(dataset_val)
 
     print(f"Faces: mean = {stats_faces[0]}, std = {stats_faces[1]}")
     print(f"Comics: mean = {stats_comics[0]}, std = {stats_comics[1]}")
