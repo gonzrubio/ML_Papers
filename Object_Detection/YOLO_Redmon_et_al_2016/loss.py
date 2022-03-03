6d6534b30a6de3d91c92c59e5df46ae769da7fea
Gonzalo Rubio <gonzrubio95@gmail.com>
2022-03-03 00:39:37 -0800

    Need to fix IoU and sample responsible detector from full oputput

--- Object_Detection/YOLO_Redmon_et_al_2016/loss.py
+++ Object_Detection/YOLO_Redmon_et_al_2016/loss.py
@@ -12,17 +12,52 @@ import torch
 import torch.nn as nn
 
 
+def IoU(bbox_pred, bbox_true):
+    """
+    Calculates intersection over union
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct labels of Bounding Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+    Returns:
+        tensor: Intersection over union for all examples
+    """
+
+    box1_x1 = bbox_pred[..., 0:1] - bbox_pred[..., 2:3] / 2
+    box1_y1 = bbox_pred[..., 1:2] - bbox_pred[..., 3:4] / 2
+    box1_x2 = bbox_pred[..., 0:1] + bbox_pred[..., 2:3] / 2
+    box1_y2 = bbox_pred[..., 1:2] + bbox_pred[..., 3:4] / 2
+
+    box2_x1 = bbox_pred[..., 0:1] - bbox_pred[..., 2:3] / 2
+    box2_y1 = bbox_pred[..., 1:2] - bbox_pred[..., 3:4] / 2
+    box2_x2 = bbox_pred[..., 0:1] + bbox_pred[..., 2:3] / 2
+    box2_y2 = bbox_pred[..., 1:2] + bbox_pred[..., 3:4] / 2
+
+    x1, y1 = torch.max(box1_x1, box2_x1), torch.max(box1_y1, box2_y1)
+    y2, x2 = torch.min(box1_x2, box2_x2), torch.min(box1_y2, box2_y2)
+
+    # set to zero if the bounding boxes do not intersect
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+
 class YOLOv1Loss(nn.Module):
     """Criterion that that frames object detection as a regression problem.
 
-    This criterion is a multi-part loss function that measures the sum of
-    squared errors between the predicted and target bounding boxes and their
-    respective class label and conditional probability.
+    This criterion is a multi-part loss function that measures the squared
+    errors between the predicted and target bounding boxes, their respective
+    confidence scores and the conditional class probability.
     """
 
-    def __init__(self, lambdas, S=7, B=2, C=20, reduction='sum'):
+    def __init__(self, lambdas=[5, 0.5], S=7, B=2, C=20, reduction='sum'):
         """Construct the criterion.
 
+        # manual rescaling weights given to the sum of squared errors of
+        # different components in the predicted and ground truth tensors
         :param S: Number of grid cells to split the image for each direction,
         defaults to 7
         :type S: int, optional
@@ -42,18 +77,10 @@ class YOLOv1Loss(nn.Module):
         self.S = S
         self.B = B
         self.C = C
-        # self.reduction = reduction
-        self.mse = nn.MSELoss(reduction=reduction)
+        self.reduction = reduction
+        self.lambda_coord, self.lamba_noobj = lambdas
 
-        # manual rescaling weights given to the sum of squared errors of
-        # different components in the predicted and ground truth tensors
-        self.lambda_obj_coord = lambdas[0]
-        self.lambda_obj_p = 1
-        self.lamba_noobj_coord = 0
-        self.lambda_noobj_p = lambdas[1]
-        self.lambda_c = 1
-
-    def forward(self, x_pred, x_true):
+    def forward(self, y_pred, y_true):
         """Apply the criterion to the predictions and ground truths.
 
         :param x_pred: Predicted tensor of shape (N, S*S*(B*5+C)).
@@ -64,17 +91,50 @@ class YOLOv1Loss(nn.Module):
         `none`, shape (N) otherwise, scalar.
         :rtype: torch.Tensor
         """
-        # Apply the functional form of operations where possible
-        x_pred = x_pred.reshape(-1, self.S, self.S, self.B * 5 + self.C)
-        # looks like doing mse of the output volume, look for efficient way
-        # to compute the volume and check how the labels are stored sparse
-        # format?
+        # Apply torch/Functional operations where possible
 
-        # maybe the nms 
-        # IoU for the B predicted and target bounding boxes
-        return x_pred
+        # find the bounding box b responsible for detecting the object
+        # by computing the confidence score of the detectors
+        conf = torch.empty(size=(y_pred.shape[0], self.S, self.S, self.B))
+        for b in range(self.B):
+            conf[...,b:b+1] = IoU(y_pred[..., b*5+1:b*5+5], y_true[..., :-1])
+            conf[...,b:b+1] *= y_pred[..., b*5:b*5+1]
+
+        # confidence = ious * 
+        detector = torch.max(conf, dim=-1)[1].unsqueeze(-1)
+
+        # mask for grid cells that contain an object
+        obj = (y_true[..., 0] > 0).int().unsqueeze(-1)
+
+        # error for the bbox center
+        # find way to construct the tensor with sampled detectors, maybe even retrace
+        # to detector. would flat detector help index??
+        loss = obj *  y_true[..., :2], y_pred[..., detector*5+1:detector*5+3]
+
+        return obj
 
 
 if __name__ == "__main__":
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    N = 1
+    S = 7
+    B = 2
+    C = 20
+    num_objects = 7
+
+    loss = YOLOv1Loss(lambdas=[5, 0.5], S=S, B=B, C=C, reduction='sum')
+
+    # dummy target batch, class is encoded as an integer in [1, num_classes]
+    y_true = torch.zeros((N, S, S, 5))
+
+    for batch in range(N):
+        for obj in torch.randint(1, S**2, (num_objects,)):
+            print(obj)
+            x, y = obj // S, obj % S        
+            y_true[batch, x, y, :-1] = torch.rand(4)
+            y_true[batch, x, y, -1] = torch.randint(1, C, (1, ))
+
+    
+    print(loss(torch.rand((N, S, S, B * 5 + C)), y_true))
