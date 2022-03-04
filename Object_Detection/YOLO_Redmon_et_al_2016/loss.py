f0dbf96eebcfb41169821219ce1ca2b27435847c
Gonzalo Rubio <gonzrubio95@gmail.com>
2022-03-03 22:07:45 -0800

    test larger batch sizes

--- Object_Detection/YOLO_Redmon_et_al_2016/loss.py
+++ Object_Detection/YOLO_Redmon_et_al_2016/loss.py
@@ -93,23 +93,17 @@ class YOLOv1Loss(nn.Module):
         `none`, shape (N) otherwise, scalar.
         :rtype: torch.Tensor
         """
-        #  mask_obj = y_true > 0   # maintains the same shape as y_true
         # produces shape [num_objects, 5 | S*S*(B*5+C)]
         mask_obj = (y_true > 0)[...,0]
         mask_noobj = torch.logical_not(mask_obj)
 
-        # y_true_obj = y_true[mask_obj].reshape(-1, 5)   # [num_objects, 5]
         y_true_obj = y_true[mask_obj]       # [num_objects, 5]
-        # y_true_noobj = y_true[mask_noobj]   # [S^2 - num_objects, 5]
-
-        # y_pred_obj = y_pred[mask_obj[..., 0]]
         y_pred_obj = y_pred[mask_obj]       # [num_objects, S*S*(B*5+C)]
         y_pred_noobj = y_pred[mask_noobj]   # [S^2 - num_objects, 5]
 
-        # now need to find max IoU between all B bbouning boxes and the targets
+        # finds detector with max IoU between all B bbouning boxes and targets
         # returns shape [num_objects, len([conf_score, cx, cy, w, h]) + C]    
         y_pred_obj = self._maxIoU(y_true_obj, y_pred_obj)
-
 
         # loss coord
         y_true_obj[:, 2:4] = torch.sqrt(y_true_obj[:, 2:4])
@@ -117,56 +111,42 @@ class YOLOv1Loss(nn.Module):
 
         lambda_coord = 5
         loss_coord = lambda_coord * F.mse_loss(
-            y_pred_obj[:, 1:5], y_true_obj[:, :4], reduction='sum'
+            y_pred_obj[:, 1:5], y_true_obj[:, :4], reduction=self.reduction
             )
-
 
         # loss confidence scores
         loss_conf_obj = F.mse_loss(
-            torch.ones_like(y_pred_obj[:, 0]), y_pred_obj[:, 0], reduction='sum'
+            torch.ones_like(y_pred_obj[:, 0]), y_pred_obj[:, 0], reduction=self.reduction
             )
 
         lambda_noobj = 0.5
         y_pred_noobj = y_pred_noobj[:, :-C].reshape(-1, 5)[:, 0]  # [-1, 5]
 
         loss_conf_noobj = lambda_noobj * F.mse_loss(
-            torch.zeros_like(y_pred_noobj), y_pred_noobj, reduction='sum'
+            torch.zeros_like(y_pred_noobj), y_pred_noobj, reduction=self.reduction
             )
-
 
         # one hot encoding loss
         loss_class = F.mse_loss(
-            F.one_hot(y_true_obj[:, -1].long(), num_classes=20),
-            y_pred_obj[..., -C:]
+            F.one_hot(y_true_obj[:, -1].long(), num_classes=20), y_pred_obj[..., -C:],
+            reduction=self.reduction
             )
 
-
-        # total loss
         loss = loss_coord + loss_conf_obj + loss_conf_noobj + loss_class
+        return loss / float(y_true.shape[0])
 
-        # # Apply torch/Functional operations where possible
+    def _maxIoU(self, y_true_obj, y_pred_obj):
+        # find the bounding box b responsible for detecting the object
+        # by computing the confidence score of the detectors
 
-        # # find the bounding box b responsible for detecting the object
-        # # by computing the confidence score of the detectors
         # conf = torch.empty(size=(y_pred.shape[0], self.S, self.S, self.B))
         # for b in range(self.B):
         #     conf[...,b:b+1] = IoU(y_pred[..., b*5+1:b*5+5], y_true[..., :-1])
         #     conf[...,b:b+1] *= y_pred[..., b*5:b*5+1]
 
-        # # confidence = ious * 
-        # detector = torch.max(conf, dim=-1)[1].unsqueeze(-1)
+        # confidence = ious * 
+        # detector = torch.max(conf, dim=-1)[1].unsqueeze(-1)        
 
-        # # mask for grid cells that contain an object
-        # obj = (y_true[..., 0] > 0).int().unsqueeze(-1)
-
-        # # error for the bbox center
-        # # find way to construct the tensor with sampled detectors, maybe even retrace
-        # # to detector. would flat detector help index??
-        # loss = obj *  y_true[..., :2], y_pred[..., detector*5+1:detector*5+3]
-
-        return loss
-
-    def _maxIoU(y_true_obj, y_pred_obj):
         return y_pred_obj[..., 0: 5+ 20]
 
 
@@ -174,7 +154,7 @@ if __name__ == "__main__":
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    N = 1
+    N = 50
     S = 7
     B = 2
     C = 20
@@ -182,8 +162,8 @@ if __name__ == "__main__":
 
     # dummy target batch [N, S, S, len([center_x, center_y, w, h, class_num])]
     # and dummy predicted volume
-    y_true = torch.zeros((N, S, S, 5))
-    y_pred = torch.rand((N, S, S, B * 5 + C))
+    y_true = torch.zeros((N, S, S, 5), device=device)
+    y_pred = torch.rand((N, S, S, B * 5 + C), device=device)
 
     for b in range(N):
         for obj in torch.randint(1, S**2, (num_objects,)):
