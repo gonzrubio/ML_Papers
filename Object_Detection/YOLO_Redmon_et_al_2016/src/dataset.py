ce6cf1c4e898272fafe9c842bdb54a3d1258bdf4
Gonzalo Rubio <gonzrubio95@gmail.com>
2022-10-31 21:30:57 -0700

    implement collate function

--- Object_Detection/YOLO_Redmon_et_al_2016/src/dataset.py
+++ Object_Detection/YOLO_Redmon_et_al_2016/src/dataset.py
@@ -1,4 +1,5 @@
-"""Custom datasets for object detection.
+# -*- coding: utf-8 -*-
+"""Custom datasets for object detection with YOLO.
 
 Created on Fri Oct 28 13:33:06 2022
 
@@ -9,13 +10,12 @@ import numpy as np
 import os
 import torch
 
-from torchvision import transforms
-from torchvision.io import read_image
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-from utils.bounding_box import encode_labels
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+# from utils.bounding_box import encode_labels, decode_labels
+from utils.transforms import Transform, AugmentTransform
 
-# from utils import plot_batch
+# from utils.plots import plot_batch
 
 
 class VOCDetection(Dataset):
@@ -57,8 +57,8 @@ class VOCDetection(Dataset):
         defaults to 'train'
         :type split: str, optional
         :param train: Training mode, returns the images and the encoded and
-        augmented ground truth labels if True, otherwise it returns the raw
-        ground truth labels, defaults to True
+        augmented ground truth labels if True, otherwise it returns the images
+        and the raw ground truth labels, defaults to True
         :type train: bool, optional
 
         """
@@ -67,11 +67,7 @@ class VOCDetection(Dataset):
         with open(os.path.join(root, split + '.txt')) as f:
             self.data = f.read().splitlines()
         self.train = train
-        # if self.train:
-        #     self.transform = transforms.Compose(
-        #         # random scaling, up to 20% translation, up to 1.5 times exposure
-        #         # and saturation in hsv color space
-        #         )
+        self.transform = AugmentTransform() if self.train else Transform()
 
     def __len__(self):
         """Return the number of samples in the dataset."""
@@ -83,24 +79,45 @@ class VOCDetection(Dataset):
         image_path = os.path.join(self.images_dir, filename + '.jpg')
         label_path = os.path.join(self.annotations_dir, filename + '.csv')
 
-        image = read_image(image_path)
+        image = Image.open(image_path).convert('RGB')
         labels = torch.from_numpy(
             np.loadtxt(label_path, skiprows=1, delimiter=',')
             )
 
+        image, labels = self.transform(image, labels)
         if self.train:
-            # TODO look into tranforms and how to use it
-            image, labels = self.transform(image, labels)
-            labels = encode_labels(labels)
+            # labels = encode_labels(labels)
+            pass
 
         return image, labels
+
+    def collate_fn(self, batch):
+        """Collate function to be passed to the DataLoader.
+
+        :param batch: An iterable of N (image, labels) pairs from __getitem__()
+        :type batch: list
+        :return: A single tensor containing the images and a list of
+        varying-size tensors with the labels
+        :rtype: tuple
+
+        """
+        images = [None] * len(batch)
+        labels = [None] * len(batch)
+
+        for idx, sample in enumerate(batch):
+            images[idx], labels[idx] = sample
+
+        images = torch.stack(images, dim=0)
+
+        return images, labels
 
 
 def voc_detection():
 
-    # get batch in eval mode (img, labels)
+    # get batch in eval mode and plot it
     data = VOCDetection(split='train', train=False)
-    dataloader = DataLoader(data, batch_size=64, shuffle=False)
+    kwargs = {'batch_size': 2, 'shuffle': False, 'collate_fn': data.collate_fn}
+    dataloader = DataLoader(data, **kwargs)
     batch = next(iter(dataloader))
     # plot_batch(batch)  # color code class and write class name on bbox
 
