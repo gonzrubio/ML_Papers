5be729a9cf53754a1b64b16306a919f363500bc1
Gonzalo Rubio <gonzrubio95@gmail.com>
2022-12-01 23:22:33 -0800

    keep track of image index for plotting later on

--- Object_Detection/YOLO_Redmon_et_al_2016/src/evaluate.py
+++ Object_Detection/YOLO_Redmon_et_al_2016/src/evaluate.py
@@ -33,8 +33,9 @@ def evaluate(model, dataloader, device, training=False):
     :rtype: tuple
 
     """
-    num_gt, num_pred = 0, 0
+    num_pred, num_gt = 0, 0
     pred_all, gt_all = torch.tensor([]), torch.tensor([])
+    img_idx_pred, img_idx_gt = [], []
 
     model.eval()
     with torch.no_grad():
@@ -47,13 +48,15 @@ def evaluate(model, dataloader, device, training=False):
             num_pred += len(pred)
             pred_all = torch.cat((pred, pred_all))
             gt_all = torch.cat((gt, gt_all))
+            img_idx_pred.extend([ii] * len(pred))
+            img_idx_gt.extend([ii] * len(gt))
 
     results = eval_metrics(pred_all, gt_all)
 
     if training:
         return num_gt, num_pred, results
 
-    return pred_all, gt_all, results
+    return pred_all, gt_all, img_idx_pred, img_idx_gt, results
 
 
 def main(config):
@@ -80,12 +83,23 @@ def main(config):
         model, dataloader, torch.device(config['device']), training=False
         )
 
-    # plot precision-recall
-    # plot threshold-F1
-    # pretty print AP and mAP
+    # plot precision-recall (implement in utils/plots, legend AP and mAP)
+    # plot threshold-F1 (legend, F1 and mF1)
     # plot all predictions
     # save plots in figures/ (write directory structure in docstring)
+    # color_palette = sns.color_palette()
+    # fig = plt.figure(figsize=(10,7))
+    # plt.plot(recall_curve, precision_curve,color=color_palette[2], lw=3)
+    # plt.grid(True)
+    # plt.xlabel("Recall")
+    # plt.ylabel("Precision")
+    # plt.title("Precision vs. Recall")
+    # plt.xlim([.5,1.05])
+    # plt.ylim([.5,1.05])
+    # plt.locator_params(axis='x', nbins=11)
+    # plt.locator_params(axis='y', nbins=11)
 
+    #     plt.savefig(str(cfg["eval_directory"].joinpath("precision_recall_%d.pdf"%class_indx)))
 
 if __name__ == "__main__":
 
