1dd86d0a65c7d00a776da44d87f9567c47fa96ac
Gonzalo <gonzrubio95@gmail.com>
2023-03-09 18:17:35 -0800

    rename files

--- Computer_Vision/Image_Transformation/Style_Transfer/CLIPstyler_Kwon_&_Ye_2022/src/CLIPstyler.py
+++ Computer_Vision/Image_Transformation/Style_Transfer/CLIPstyler_Kwon_&_Ye_2022/src/CLIPstyler.py
@@ -9,6 +9,9 @@ Created on Wed Feb 22 21:24:31 2023
 @author: gonzalo
 """
 
+import clip
+import torch
+
 # with torch.no_grad():
 #     img_transformed = preprocess(img)
 #     content_image = preprocess("path/to/content/image.jpg")
@@ -40,14 +43,30 @@ def feature_maps(image, model):
     return features
 
 
-def stylize(img_c, txt):
-    # get device
-    # model = StyleNet()
-    # for n_iter (look at paper)
-    # forward
-    # losses
+def stylize(img_c, txt, device):
+    # modularize as much a spossible
+    lambda_d = 5e2
+    lambda_p = 9e3
+    lamda_c = 150
+    lamda_tv = 2e-3
+    # stylenet = StyleNet()
+    # optimizer = optim.Adam(lr=5e-4)
+    # scheduler decrease to half at iteration 100
+    # for n_iter=200
+    # text_tensors = clip.tokenize(texts).to(device)
+    # txt = mean(clip(txt prompts))
+    # function to get embeddings (vgg, clip txt img)
+    # img_cs = stylenet(img_c)
+    # loss_dir = lambda_d * () # for semantic information
+    # loss_patch = lambda_p * () # spatially invariant texture
+    # randomly crop n=64 patches and apply random geometrical (perspective sclae=0.5) augmentations and calculate clip loss
+    # patch size 128
+    # threshold rejection tau=0.7 (nullify loss for patches above some threshold)
+    # loss_content = lamda_c * MSE(img_c_vgg, img_cs_vgg)
+    # loss_tv = lamda_tv * ()
+    # loss_total = loss_dir + loss_patch + loss_content + loss_tv
+    # display total and infividual losses
     # update weights
-    # display loss
 
     return img_cs
 
@@ -58,14 +77,16 @@ def load_vgg_clip():
     vgg = vgg19_bn(weights=weights)
     vgg.eval()
     # load clip (from transformers or opeinai)
-    return vgg, preprocess_vgg, clip, preprocess_clip
+    model, preprocess = clip.load('ViT-B/32', device, jit=False) # why false?
+
+    return vgg, preprocess_vgg, clip, preprocess_clip (224x224)
 
 
 def get_dataloader(src):
     transform = transforms.Compose([
         transforms.Resize(512),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))????
         ])
     # load images with bs=1 in torchvision Image class
     return dataloader
@@ -78,16 +99,17 @@ def get_conditions(src):
 def main(cfg):
     """Text-guided style transfer pipeline."""
     # get text, images and pre-trained models
-    conditions = get_conditions(cfg['text'])
+    conditions = get_conditions(cfg['text']) # flexible to take in many or just one
     dataloader = get_dataloader(cfg['content'])
     vgg, preprocess_vgg, clip, preprocess_clip = load_vgg_clip()
-
     for name, module in model.named_modules():
         print(name, type(module))
-
+    # get device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     # for each image in loader tqdm
     # for each promtp tqdm (look up nice format nested tqdm)
-    # img_cs = stylize(img_c, txt)
+    # img_cs = stylize(img_c, txt, device)
+    # contrast enhancements?
     # save img_c_txt.png (full res output/)
     # append to collage list
 
@@ -95,8 +117,7 @@ def main(cfg):
 
 
 if __name__ == '__main__':
-    # TODO look at style transfer favorite script
-    # TODO look at paper and run on a single image-text condition pair
+    # TODO look at paper and run on a single image-text condition (x10) pair
     # TODO see how authors use pre-trained models
     # TODO make content_images/ (x7) and make text_conditions/ (x7)
     # TODO read args and set defaults
