52305105c73fe3362d9fdd93cae74f505a9392c0
Gonzalo <gonzrubio95@gmail.com>
2023-03-13 23:27:21 -0700

    compute directional CLIP loss

--- Computer_Vision/Image_Transformation/Style_Transfer/CLIPstyler_Kwon_&_Ye_2022/src/CLIPstyler.py
+++ Computer_Vision/Image_Transformation/Style_Transfer/CLIPstyler_Kwon_&_Ye_2022/src/CLIPstyler.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 
 import torch
 import torch.optim as optim
-import torchvision.transforms as transforms
+import torchvision.transforms as T
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import Dataset, DataLoader
 from torchvision.models import vgg19, VGG19_Weights
@@ -27,19 +27,12 @@ import matplotlib.pyplot as plt
 from StyleNet import StyleNet
 
 
-def feature_maps(image, model):
+def vgg_feature_maps(image, model):
 
-    layers = {'0': 'conv1_1',
-              '5': 'conv2_1',
-              '10': 'conv3_1',
-              '19': 'conv4_1',
-              '21': 'conv4_2',
-              '28': 'conv5_1',
-              '31': 'conv5_2'}
-
+    layers = {'21': 'conv4_2', '31': 'conv5_2'}
     features = {}
     x = image
-    # x = x.unsqueeze(0)
+    x = x.unsqueeze(0)
     for name, layer in model._modules.items():
         x = layer(x)   
         if name in layers:
@@ -48,95 +41,88 @@ def feature_maps(image, model):
     return features
 
 
-# with torch.no_grad():
-#     img_transformed = preprocess(img)
-#     content_image = preprocess("path/to/content/image.jpg")
-#     features = model(content_image)
+def stylize(img_c, txt, models, transforms, device):
 
-#     features4_2 = model.features[:23](content_image)
-#     features5_2 = model.features[:30](content_image)
-#     content_loss = F.mse_loss(features4_2, features4_2) + F.mse_loss(features5_2, features5_2)
+    clip = models['clip'].to(device)
+    vgg = models['vgg'].to(device)
+    stylenet = StyleNet().to(device)
 
+    # Prepare the inputs and get features
+    token_t_sty = torch.cat([openaiclip.tokenize(i) for i in txt]).to(device)
+    token_src = openaiclip.tokenize('Photo').to(device)
+    I_clip = transforms['clip'](img_c).to(device)   # img_c as tensor and clip transforms
+    I_vgg = transforms['vgg'](img_c).to(device)     # img_c as tensor and vgg transforms
+    I_c = transforms['stylenet'](img_c).to(device)  # img_c as tensor and stylenet transforms
 
-def stylize(img_c, txt, vgg, preprocess_vgg, clip, preprocess_clip, device):
-    # modularize as much a spossible
+    with torch.no_grad():
+        t_sty_features = clip.encode_text(token_t_sty).mean(dim=0, keepdim=True)
+        t_src_features = clip.encode_text(token_src).mean(dim=0, keepdim=True)
+        I_c_features = clip.encode_image(I_clip.unsqueeze(0))
+        vgg_features = vgg_feature_maps(I_vgg, vgg)
+
+    # optimization parameters
+    optimizer = optim.Adam(stylenet.parameters(), lr=5e-4)
+    scheduler = StepLR(optimizer, step_size=100, gamma=0.5)
+
     lambda_d = 5e2
     lambda_p = 9e3
     lamda_c = 150
     lamda_tv = 2e-3
 
-    stylenet = StyleNet()
-    optimizer = optim.Adam(stylenet.parameters(), lr=5e-4)
-    scheduler = StepLR(optimizer, step_size=100, gamma=0.5)
-
     for i in range(200):
-        # modularize below in function to get embeddings (vgg, clip txt img)??
 
-        # Prepare the inputs
-        # TODO vgg inputs?
-        # TODO image and (avg) tokenized text conditions embeddings
-        text_tokens = torch.cat([openaiclip.tokenize(i) for i in txt]).to(device)
+        # directional CLIP loss
+        I_cs = stylenet(I_c)
+        I_cs = transforms['clip'](T.ToPILImage()(I_cs.squeeze(0))).to(device)
+        I_cs_features = clip.encode_image(I_cs.unsqueeze(0))
 
-        # TODO Calculate clip and vgg features??
-        with torch.no_grad():
-            image_features = clip.encode_image(image_input)
-            text_features = clip.encode_text(text_tokens).mean(dim=0)
+        delta_I = I_cs_features - I_c_features
+        delta_T = t_sty_features - t_src_features
+        loss_dir = lambda_d * (1 - torch.cosine_similarity(delta_I, delta_T))
 
-        # Forward pass and losses
-        img_cs = stylenet(img_c)
-        # loss_dir = lambda_d * () # for semantic information
-        # loss_patch = lambda_p * () # spatially invariant texture
+        # PatchCLIP loss
         # randomly crop n=64 patches and apply random geometrical (perspective sclae=0.5) augmentations and calculate clip loss
         # patch size 128
         # threshold rejection tau=0.7 (nullify loss for patches above some threshold)
+        # loss_patch = lambda_p * ()
+
         # loss_content = lamda_c * MSE(img_c_vgg, img_cs_vgg)
+
         # loss_tv = lamda_tv * ()
+
         # loss_total = loss_dir + loss_patch + loss_content + loss_tv
+
         # Print the losses
         print(f"Iteration {i}: total loss = {total_loss.item()}, "
               f"dir loss = {loss_dir.item()}, patch loss = {loss_patch.item()}, "
               f"content loss = {loss_content.item()}, TV loss = {loss_tv.item()}")
 
-        # optimizer.zero_grad()
-        # loss.backward()
-        # optimizer.step()
-        # scheduler.step()
-
     return img_cs
 
 
-def load_vgg_clip():
+def get_models_transforms():
 
     weights = VGG19_Weights.DEFAULT
     preprocess_vgg = weights.transforms()
-    vgg = vgg19(weights=weights)
+    vgg = vgg19(weights=weights).features
     vgg.eval()
 
     clip, preprocess_clip = openaiclip.load('ViT-B/32', jit=False)
     clip.eval()
+    
+    models = {'vgg': vgg, 'clip': clip}
 
-    return vgg, preprocess_vgg, clip, preprocess_clip
+    transforms = {
+        'stylenet': T.Compose([
+            T.Resize((512, 512)),
+            T.ToTensor(),
+            lambda x: torch.unsqueeze(x, 0)
+            ]),
+        'clip': preprocess_clip,
+        'vgg': preprocess_vgg,
+        }
 
-
-def get_dataloader(src):
-
-    class ImageFolder(Dataset):
-        def __init__(self, data_path):
-            self.data_path = data_path
-            self.transforms = transforms.Compose([transforms.Resize((512, 512)),
-                                                  transforms.ToTensor()])
-            self.images = []
-            for sample_name in os.listdir(self.data_path):
-                sample_path = os.path.join(self.data_path, sample_name)
-                self.images.append(sample_path)
-
-        def __getitem__(self, idx):
-            return self.transforms(Image.open(self.images[idx]))
-
-        def __len__(self):
-            return len(self.images)
-
-    return DataLoader(ImageFolder(src))
+    return models, transforms
 
 
 def get_conditions(src):
@@ -148,27 +134,23 @@ def get_conditions(src):
 def main(cfg):
     """Text-guided style transfer pipeline."""
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
     conditions = get_conditions(cfg['text'])
-    dataloader = get_dataloader(cfg['content'])
-    vgg, preprocess_vgg, clip, preprocess_clip = load_vgg_clip()
+    models, transforms = get_models_transforms()
 
-    for img_c in tqdm(dataloader):
+    for img_c in tqdm(os.listdir(cfg['content'])):
+        img_c = Image.open(os.path.join(cfg['content'], img_c))
         for txt in tqdm(conditions, leave=False):
-            img_cs = stylize(
-                img_c, txt, vgg, preprocess_vgg, clip, preprocess_clip, device
-                )
+            img_cs = stylize(img_c, txt, models, transforms, device)
             # contrast enhancements
             # save img_c_txt.png
             # append to collage list
-
     # plot all images as table (or two depending on size, fix num images per grid) like fig 1 (highest res possible output/)
 
 
 if __name__ == '__main__':
     # TODO run end-to-end on a single image-text pair
-    # TODO more text_conditions/ (x7) and plots
     # TODO Look at official code
+    # TODO more text_conditions/ (x7) and plots
     # TODO read args and set defaults
     cfg= {
         'content': os.path.join(os.getcwd(), '..', 'data', 'content_images'),
