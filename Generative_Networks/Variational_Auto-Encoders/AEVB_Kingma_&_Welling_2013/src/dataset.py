3d7247ccfe8c39e4bd9336ee7e8ab90427e750b1
Gonzalo <gonzrubio95@gmail.com>
2023-04-16 15:54:52 -0700

    load the frey face dataset after downloading

--- Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/dataset.py
+++ Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/dataset.py
@@ -9,36 +9,44 @@ Created on Sun Apr 16 15:25:17 2023
 
 import os
 import urllib.request
+import torch
+import scipy.io as sio
 
 
-def download_frey_face_dataset(data_dir):
-    """
-    Downloads the Frey Face dataset and saves it to disk.
+def load_frey_face_dataset(data_dir):
+    """Loads the Frey Face dataset from a .mat file and returns a PyTorch tensor.
+
+    The Frey Face dataset is a collection of 1965 images of human faces. Each
+    image is grayscale and has dimensions 28 x 20 pixels.
 
     Parameters
     ----------
     data_dir : str
-        Path to the directory where the dataset will be saved.
+        Path to the directory where the dataset is saved or will be saved.
 
     Returns
     -------
-    None
+    data : torch.Tensor
+        A PyTorch tensor containing the Frey Face dataset. The tensor has shape
+        (n_samples, 1, 28, 20), where n_samples is the number of images in the
+        dataset.
     """
-
-    # URL for downloading the Frey Face dataset
-    url = 'https://cs.nyu.edu/~roweis/data/frey_rawface.mat'
-
-    # Create the directory if it doesn't exist
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    # File path for saving the dataset
     file_path = os.path.join(data_dir, 'frey_rawface.mat')
+    url = 'https://cs.nyu.edu/~roweis/data/frey_rawface.mat'
 
-    # Download the dataset if it doesn't exist
     if not os.path.exists(file_path):
         print('Downloading Frey Face dataset...')
         urllib.request.urlretrieve(url, file_path)
         print('Download complete.')
     else:
         print('Frey Face dataset already downloaded.')
+
+    data = sio.loadmat(file_path)
+    data = data['ff'].T.reshape(-1, 1, 28, 20)
+    data = torch.from_numpy(data).float()
+
+    return data
+
