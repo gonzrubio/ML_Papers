f34986b7510453b0f47b6d5512523e04b854beaf
Gonzalo <gonzrubio95@gmail.com>
2023-04-16 16:05:32 -0700

    download and load mnist as a tensor

--- Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/dataset.py
+++ Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/dataset.py
@@ -9,8 +9,9 @@ Created on Sun Apr 16 15:25:17 2023
 
 import os
 import urllib.request
-import torch
 import scipy.io as sio
+import torch
+import torchvision.datasets as datasets
 
 
 def load_frey_face_dataset(data_dir):
@@ -50,3 +51,34 @@ def load_frey_face_dataset(data_dir):
 
     return data
 
+
+def load_mnist_dataset(data_dir):
+    """Loads the MNIST dataset and returns a PyTorch tensor.
+
+    The MNIST dataset consists of 28 x 28 grayscale images of handwritten digits
+    (0-9). There are 60,000 training images and 10,000 test images.
+
+    Parameters
+    ----------
+    data_dir : str
+        Path to the directory where the dataset is saved or will be saved.
+
+    Returns
+    -------
+    data : torch.Tensor
+        A PyTorch tensor containing the MNIST dataset. The tensor has shape
+        (n_samples, 1, 28, 28), where n_samples is the number of images in the
+        dataset.
+    """
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    trainset = datasets.MNIST(root=data_dir, train=True, download=True)
+    testset = datasets.MNIST(root=data_dir, train=False, download=True)
+
+    train_data = trainset.data.unsqueeze(1).float() / 255.0
+    test_data = testset.data.unsqueeze(1).float() / 255.0
+
+    data = torch.cat([train_data, test_data], dim=0)
+
+    return data
