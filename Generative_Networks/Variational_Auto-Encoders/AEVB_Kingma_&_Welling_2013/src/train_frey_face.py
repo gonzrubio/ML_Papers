b8f962a1048e4301c6fb56780f1422e4e52c8e03
Gonzalo <gonzrubio95@gmail.com>
2023-04-18 18:38:05 -0700

    use Adam and Karapathy constant

--- Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/train_frey_face.py
+++ Generative_Networks/Variational_Auto-Encoders/AEVB_Kingma_&_Welling_2013/src/train_frey_face.py
@@ -26,19 +26,18 @@ def train(model, dataloader, optimizer, epochs, device):
             x = x.to(device)
             x_hat, mu, twicelogvar = model(x)
 
-            # BCE = F.binary_cross_entropy(x_hat, x, reduction= 'sum')
-            MSE = F.mse_loss(x, x_hat, reduction='sum')
+            BCE = F.binary_cross_entropy(x_hat, x, reduction= 'sum')
             KLD = torch.sum(1 + twicelogvar - mu.pow(2) - twicelogvar.exp(), dim=-1)
             KLD = torch.sum(-0.5 * KLD)
-            batch_loss = MSE + KLD
+            batch_loss = BCE + KLD
 
             optimizer.zero_grad(set_to_none=True)
             batch_loss.backward()
             optimizer.step()
 
             print(f'{epoch}.{batch_idx}',
-                  f'ELBO:{batch_loss.item(): .4f}, '
-                  f'MSE:{MSE.item(): .4f}, '
+                  f"(-'ve)ELBO:{batch_loss.item(): .4f}, "
+                  f'BCE:{BCE.item(): .4f}, '
                   f'KLD:{KLD.item(): .2e}')
 
         if epoch % 500 == 0:
@@ -76,7 +75,8 @@ def main(cfg):
         pin_memory=True
         )
 
-    optimizer = torch.optim.Adagrad(model.parameters(), lr=cfg['lr'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg['lr'])
+    # optimizer = torch.optim.Adagrad(model.parameters(), lr=cfg['lr'])
 
     model = train(model, dataloader, optimizer, cfg['epochs'], device)
     print(model)
@@ -93,7 +93,7 @@ if __name__ == '__main__':
         'batch_size': 100,
         'hidden_size': 200,
         'latent_size': 2,
-        'lr': 4e-5,
-        'epochs': 35000
+        'lr': 3e-4,
+        'epochs': 10000
         }
     main(cfg)
